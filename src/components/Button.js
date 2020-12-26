94ad5824564cacf4811b96dd69d283f120360c12
Gading <sutan.gnst@gmail.com>
2020-12-26 11:06:04 +0700

    nambahin props text untuk text dinamis

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,15 @@
 import React from 'react'
 
 class Button extends React.Component {
+  static defaultProps = {
+    text: ''
+  }
+
   render() {
+    const { text } = this.props
     return (
       <button>
-        Buttton
+        {text}
       </button>
     )
   }
